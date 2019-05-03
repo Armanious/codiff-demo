d968236a284c1b03eb515f7c7bad61b2335f842d
Armanious <armanious@live.com>
2019-05-03 14:06:45 -0400

    fizzbuzz blank

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,8 +3,9 @@
 '''
 For all numbers in [1, n],
 if the number is divisible by 3, print "<number> fizz",
-if the number is divisible by 5, print "<number> buzz", and
-if the number is divisible by both 3 and 5, print "<number> fizzbuzz".
+if the number is divisible by 5, print "<number> buzz",
+if the number is divisible by both 3 and 5, print "<number> fizzbuzz",
+otherwise print "<number>"
 '''
 def fizzbuzz(n):
     pass
